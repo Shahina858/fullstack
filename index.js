660c08cb857d2347ab631d490e9672ef8bf59ce2
Shahina858 <mshahina480@gmail.com>
2025-10-20 10:58:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://fullstack-frontend.vercel.app", 
+    methods: ["GET","POST","PUT","DELETE"]
+}));
 
 const bookRoute = require("./routes/bookroutes");
 app.use("/api/books", bookRoute);
